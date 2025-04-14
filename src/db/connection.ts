71e54f63ffb7d9a03bd16a729ca1fd112fe8c85b
oscarrep <oscar.bcn.95@gmail.com>
2025-04-14 12:42:02 +0200

    update dotenv config + checking database connection

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config({
+    path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
+});
 
 const sequelize = new Sequelize(
     process.env.DB_NAME as string,
@@ -13,5 +15,24 @@ const sequelize = new Sequelize(
         logging: true,
     }
 );
+
+if(process.env.NODE_ENV === 'production'){
+sequelize.authenticate()
+  .then(() => {
+    console.log("Connected to Railway MySQL!");
+  })
+  .catch(err => {
+    console.error("Unable to connect:", err);
+  });
+}
+else if(process.env.NODE_ENV === 'development'){
+sequelize.authenticate()
+  .then(() => {
+    console.log("Connected to Local MySQL!");
+  })
+  .catch(err => {
+    console.error("Unable to connect:", err);
+  });
+}
 
 export default sequelize;
