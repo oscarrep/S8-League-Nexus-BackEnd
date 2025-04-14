1678097e132267918362cadaa77ccdfcb4db7808
oscarrep <oscar.bcn.95@gmail.com>
2025-04-14 05:56:06 +0200

    update database connection to use MySQL environment variables for railway

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,12 +2,12 @@ import { Sequelize } from "sequelize";
 import dotenv from 'dotenv'
 dotenv.config();
 const sequelize = new Sequelize(
-    process.env.DB_NAME as string,
-    process.env.DB_USER as string,
-    process.env.DB_PASSWORD as string,
+    process.env.MYSQLDATABASE || process.env.DB_NAME || '',
+    process.env.MYSQLUSER || process.env.DB_USER || '' ,
+    process.env.MYSQLPASSWORD || process.env.DB_PASSWORD || '' ,
     {
-        host: process.env.DB_HOST as string,
-        port: Number(process.env.PORT),
+        host: process.env.MYSQLHOST || process.env.DB_HOST || 'localcost' ,
+        port: Number(process.env.MYSQLPORT) || Number(process.env.DB_PORT) || 3306,
         dialect: 'mysql',
         logging: true
     }
