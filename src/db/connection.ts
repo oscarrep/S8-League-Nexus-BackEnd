c69fa5e6062678d92062c83ed12161a212e6a72a
oscarrep <oscar.bcn.95@gmail.com>
2025-04-14 06:57:26 +0200

    add railway variables

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,15 +1,17 @@
 import { Sequelize } from "sequelize";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 dotenv.config();
+
 const sequelize = new Sequelize(
-    process.env.DB_NAME || '',
-    process.env.DB_USER || '' ,
-    process.env.DB_PASSWORD || '' ,
-    {
-        host: process.env.DB_HOST || 'localhost' ,
-        dialect: 'mysql',
-        logging: true
-    }
+  process.env.DB_NAME || process.env.MYSQLDATABASE || '',
+  process.env.DB_USER || process.env.MYSQLUSER || '',
+  process.env.DB_PASSWORD || process.env.MYSQLPASSWORD || '',
+  {
+    host: process.env.DB_HOST || process.env.MYSQLHOST || 'localhost',
+    port: Number(process.env.DB_PORT || process.env.MYSQLPORT) || 3306,
+    dialect: 'mysql',
+    logging: console.log
+  }
 );
 
 export default sequelize;
