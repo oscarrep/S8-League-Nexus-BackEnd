4995dfb4d2a47734cf9e75277fd338fb0525d6f5
oscarrep <oscar.bcn.95@gmail.com>
2025-04-16 18:59:38 +0200

    refactor database connection logic

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,33 +1,38 @@
 import { Sequelize } from "sequelize";
 import dotenv from 'dotenv';
+
 dotenv.config({
   path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
 });
 
-const sequelize = new Sequelize(
-  process.env.MYSQL_URL as string, {
-  dialect: 'mysql',
-  logging: true,
-}
-);
+let sequelize: Sequelize;
 
 if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(process.env.MYSQL_URL as string, {
+    dialect: 'mysql',
+    logging: true,
+  });
+
   sequelize.authenticate()
-    .then(() => {
-      console.log("Connected to Railway MySQL!");
-    })
-    .catch(err => {
-      console.error("Unable to connect:", err);
-    });
-}
-else if (process.env.NODE_ENV === 'development') {
+    .then(() => console.log("Connected to Railway MySQL!"))
+    .catch(err => console.error("Unable to connect to Railway:", err));
+
+} else {
+  sequelize = new Sequelize(
+    process.env.MYSQLNAME as string,
+    process.env.MYSQLUSER as string,
+    process.env.MYSQLPASSWORD as string,
+    {
+      host: process.env.MYSQLHOST as string,
+      port: Number(process.env.MYSQLPORT),
+      dialect: 'mysql',
+      logging: true,
+    }
+  );
+
   sequelize.authenticate()
-    .then(() => {
-      console.log("Connected to Local MySQL!");
-    })
-    .catch(err => {
-      console.error("Unable to connect:", err);
-    });
+    .then(() => console.log("Connected to Local MySQL!"))
+    .catch(err => console.error("Unable to connect to Local MySQL:", err));
 }
 
 export default sequelize;
